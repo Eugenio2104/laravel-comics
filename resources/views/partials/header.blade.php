f06fea53c55e972688aad05b716b7d3ad1160278
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-12-22 17:59:16 +0100

    inserimento header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,24 @@
 <header>
-    <div class="logo">
-        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo" />
+    <div class="container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo" />
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">characters</a>
+                    <a href="#">comics</a>
+                    <a href="#">movies</a>
+                    <a href="#">tv</a>
+                    <a href="#">games</a>
+                    <a href="#">collectibles</a>
+                    <a href="#">videos</a>
+                    <a href="#">fans</a>
+                    <a href="#">news</a>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </header>
